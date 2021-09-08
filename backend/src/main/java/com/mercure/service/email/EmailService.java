1d048c14ac38672b32cbf1d4275e4c622862b402
Rahmo Huseinagic <45545657+rahmohuseinagic@users.noreply.github.com>
2021-09-08 16:48:55 +0200

    Added certificate and smtp server utility

---
+++ backend/src/main/java/com/mercure/service/email/EmailService.java
@@ -0,0 +1,38 @@
+package com.mercure.service.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+@Component
+@Service
+public class EmailService {
+
+    //@Qualifier("getJavaMailSender")
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendMessage(String to, String pathToAttachment) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(to);
+        helper.setSubject("Chat Controller - Verification email");
+        helper.setText("Congratulations. You have been successfully registered.\n Please download and save" +
+                "this certificate and use it to login.");
+
+        FileSystemResource file
+                = new FileSystemResource(new File(pathToAttachment));
+        helper.addAttachment("Certificate", file);
+
+        emailSender.send(message);
+    }
+}
